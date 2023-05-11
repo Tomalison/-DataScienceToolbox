3b8dad73f88c1249f71a4cfdcbff3b29b40f6a77
Tomalison <96727036+Tomalison@users.noreply.github.com>
2023-05-11 22:33:32 +0800

    專案一

--- 鐵達尼號的存活預測.py
+++ 鐵達尼號的存活預測.py
@@ -25,11 +25,19 @@ fig=plt.gcf()
 fig.set_size_inches(10,8)
 plt.show()
 #類別欄位與是否生存欄位關係
+#轉換類別欄位為數值
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+df_class['Name'] = labelencoder.fit_transform(df_class['Name'])
+df_class['Ticket'] = labelencoder.fit_transform(df_class['Ticket'].astype(str))
+df_class['Cabin'] = labelencoder.fit_transform(df_class['Cabin'].astype(str))
+df_class['Embarked'] = labelencoder.fit_transform(df_class['Embarked'].astype(str))
+df_class['PassengerId'] = labelencoder.fit_transform(df_class['PassengerId'].astype(str))
 g = sns.heatmap(df_class.corr(),annot=True,cmap='RdYlGn',linewidths=0.2)
 fig=plt.gcf()
 fig.set_size_inches(10,8)
 plt.show()
 
 #承上題，請問你是如何定義與解讀什麼稱為「關係大」呢？
-'''數值欄位與是否生存欄位關係:Age與Survived關係最大，其次是Fare與Survived關係最大
-    類別欄位與是否生存欄位關係:Ticket與Survived關係最大，其次是Cabin與Survived關係最大'''
+'''數值欄位與是否生存欄位關係:Survived與數值的相關係數絕對值最大為0.26，數值欄位與是否生存欄位關係不夠大
+    類別欄位與是否生存欄位關係:Survived與類別的相關係數絕對值最大為0.25(客艙)，類別欄位與是否生存欄位關係不夠大'''
